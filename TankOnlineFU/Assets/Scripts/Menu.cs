99ce29671f945fe9d0f092f0cfff501624cdbca2
Lương Vũ <90044650+Dailuong11@users.noreply.github.com>
2023-07-11 21:29:16 +0700

    map

--- TankOnlineFU/Assets/Scripts/Menu.cs
+++ TankOnlineFU/Assets/Scripts/Menu.cs
@@ -1,43 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public void Play()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+   
 
-    public void LoadMap()
+    public void Map()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-    }
-    public void ConstructionMap()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
-    }
-    public void Load_Map1()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
-    }
-    public void Load_Map2()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
-    }
-    public void Load_Map3()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 6);
-    }
-    public void Load_Map4()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 7);
-    }
-    public void Load_Map()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 8);
+        //SceneManager.LoadScene("Menu");
     }
     public void Return()
     {
@@ -45,10 +18,23 @@ public class Menu : MonoBehaviour
     }
     public void ReturnConstruction()
     {
-        SceneManager.LoadScene("Construction");
+        SceneManager.LoadScene("Menu");
+    }
+    public void SaveMap()
+    {
+        SceneManager.LoadScene("SaveMap");
+    }
+    public void Play()
+    {
+       int id = PlayerPrefs.GetInt("id");
+        SceneManager.LoadScene("Play");
+    }
+    public void LoadMap()
+    {
+        SceneManager.LoadScene("LoadMap");
     }
     public void QuitGame()
     {
-        Application.Quit();
+        SceneManager.LoadScene("First_Map");
     }
 }
